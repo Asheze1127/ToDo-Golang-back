58bffd4558e8777e7d756197172b0042ffb8f7e6
Asheze1127 <m.k.h11272005@gmail.com>
2025-10-06 02:20:46 +0900

    move to infrastructure

---
+++ internal/infrastructure/db/db.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"github.com/joho/godotenv"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	_ = godotenv.Load()
+
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		panic("DATABASE_URL is not set")
+	}
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil{
+		panic("Failed to connect to database")
+	}
+
+	fmt.Println("✅Connected to Render PostgresSQL")
+}
