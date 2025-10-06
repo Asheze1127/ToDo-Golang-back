0957b805cc060a290224b75c52102a088f103087
Asheze1127 <m.k.h11272005@gmail.com>
2025-10-06 11:43:19 +0900

    jwt token

---
+++ internal/util/jwt.go
@@ -0,0 +1,18 @@
+package util
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var secretKey = []byte("your_secret_key")
+
+func GenerateJWT(userID string) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(24 * time.Hour).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(secretKey)
+}
