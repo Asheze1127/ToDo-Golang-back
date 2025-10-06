4dd1157a7a8905a067386853ed51e8b0bb561dd6
Asheze1127 <m.k.h11272005@gmail.com>
2025-10-06 11:17:14 +0900

    fix user models

--- internal/models/user.go
+++ internal/models/user.go
@@ -3,7 +3,6 @@ package models
 import (
 	"time"
 	"github.com/google/uuid"
-	"gorm.io/gorm"
 )
 
 type User struct {
@@ -12,5 +11,4 @@ type User struct {
 	Password  string    `gorm:"not null" json:"password"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
 }
